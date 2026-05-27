8bc08a17d221e2b60fbd36368b3cbc7b4029fcc7
OtavioUhuuu <otavio.moraes.soares@escola.pr.gov.br>
2026-05-27 13:59:51 -0300

    Refactor button click event to check for elements

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ avanca.forEach(button => {
         const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-        atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
+        if (atual) {
+            atual.classList.remove('ativo');
+        }
+
+        const proximoElemento = document.getElementById(proximoPasso);
+        if (proximoElemento) {
+            proximoElemento.classList.add('ativo');
+        }
     });
 });
